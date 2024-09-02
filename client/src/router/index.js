7c1dea2efc79a645b4a3e0ea074241a18269f162
sharyar <131683156+sharyarnaveed@users.noreply.github.com>
2024-09-03 04:54:42 +0500

    sign in page done

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Signup from "@/views/signup.vue"
+import Signin from "@/views/signin.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -13,6 +14,11 @@ const router = createRouter({
     path:"/signup",
     name:"signup",
     component:Signup
+  },
+  {
+    path:"/signin",
+    name:"signin",
+    component:Signin
   }
   ]
 })
